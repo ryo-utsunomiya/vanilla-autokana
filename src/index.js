2f47b42204eb22b9b5b0ffbdefb60167ee4d47ad
Ryo Utsunomiya <millionsnowliving@gmail.com>
2018-03-31 13:43:50 +0900

    Arrange basic build pipeline

---
+++ src/index.js
@@ -0,0 +1,8 @@
+/**
+ * @param {String} userNameInput
+ * @param {String} userFuriganaInput
+ * @param {Object} option
+ */
+export default function autoKana(userNameInput, userFuriganaInput, option) {
+  console.log('autoKana');
+};
