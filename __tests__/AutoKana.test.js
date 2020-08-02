742bd279c9d74885d83dbcdaef80b620f3a9cafd
Kazuki Nishikawa <kz.nishikawa@gmail.com>
2020-08-02 13:57:42 +0900

    feat: accept Element objets for AutoKana.bind arguments

--- __tests__/AutoKana.test.js
+++ __tests__/AutoKana.test.js
@@ -10,3 +10,15 @@ test('init', () => {
   autokana.start();
   expect(autokana.isActive).toBe(true);
 });
+
+test('init with pass elements', () => {
+  document.body.innerHTML = `
+<input name="name" id="name">
+<input name="furigana" id="furigana">
+`;
+  const name = document.getElementById('name')
+  const furigana = document.getElementById('furigana')
+  const autokana = new AutoKana(name, furigana);
+  autokana.start();
+  expect(autokana.isActive).toBe(true);
+});
--- types/autokana.d.ts
+++ types/autokana.d.ts
@@ -1,16 +1,17 @@
 declare module 'vanilla-autokana' {
+  type Bindable = string | Element
   export interface Option {
     katakana: boolean;
     debug: boolean;
     checkInterval: number;
   }
   export function bind(
-    name: string,
-    furigana?: string,
+    name: Bindable,
+    furigana?: Bindable,
     option?: Partial<Option>,
   ): AutoKana;
   class AutoKana {
-    constructor(name: string, furigana?: string, option?: Partial<Option>);
+    constructor(name: Bindable, furigana?: Bindable, option?: Partial<Option>);
     public getFurigana(): string;
     public start(): void;
     public stop(): void;
